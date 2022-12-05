a04ebb07103bc50d7d2745f02645ea59c1f9e0c6
fernandojerez <fernandojerez@gmail.com>
2022-12-04 21:50:48 -0500

    add ksharp lexer

---
+++ parser/src/main/kotlin/ksharp/parser/KSharpLexer.kt
@@ -0,0 +1,117 @@
+package ksharp.parser
+
+enum class KSharpTokenType : TokenType {
+    UpperCaseWord,
+    LowerCaseWord,
+    Integer,
+    Float,
+    Alt,
+    OpenBracket,
+    CloseBracket,
+    OpenParenthesis,
+    CloseParenthesis,
+    OpenCurlyBraces,
+    CloseCurlyBraces,
+    WhiteSpace,
+    NewLine,
+
+    Operator,
+}
+
+private val mappings = mapOf(
+    '@' to KSharpTokenType.Alt,
+    '[' to KSharpTokenType.OpenBracket,
+    ']' to KSharpTokenType.CloseBracket,
+    '(' to KSharpTokenType.OpenParenthesis,
+    ')' to KSharpTokenType.CloseParenthesis,
+    '{' to KSharpTokenType.OpenCurlyBraces,
+    '}' to KSharpTokenType.CloseCurlyBraces
+)
+
+private val operators = "+-*/%><=!&$#^?.\\|".toSet()
+
+fun Char.isNewLine() = this == '\n'
+fun Char.isOperator() = operators.contains(this)
+fun Char.isDot() = this == '.'
+
+fun Lexer.operator(): LexerToken {
+    while (true) {
+        val c = this.nextChar() ?: return token(KSharpTokenType.Operator, 1)
+        if (!c.isOperator()) {
+            return token(KSharpTokenType.Operator, 1)
+        }
+    }
+}
+
+fun Lexer.word(type: TokenType): LexerToken {
+    while (true) {
+        val c = this.nextChar() ?: return token(type, 1)
+        val value = c.isLetter() || c.isDigit() || c == '_'
+        if (!value) {
+            return token(type, 1)
+        }
+    }
+}
+
+fun Lexer.number(): LexerToken {
+    while (true) {
+        val c = this.nextChar() ?: return token(KSharpTokenType.Integer, 1)
+        if (c.isDot()) return decimal(KSharpTokenType.Integer, 2)
+        if (!c.isDigit()) {
+            return token(KSharpTokenType.Integer, 1)
+        }
+    }
+}
+
+fun Lexer.decimal(type: TokenType, skip: Int): LexerToken {
+    var start = false
+    while (true) {
+        val c = this.nextChar() ?: return token(type, skip)
+        if (!c.isDigit()) {
+            if (!start) return token(type, skip)
+            return token(KSharpTokenType.Float, 1)
+        }
+        start = true
+    }
+}
+
+
+fun Lexer.newLine(): LexerToken {
+    while (true) {
+        val c = this.nextChar() ?: return token(KSharpTokenType.NewLine, 1)
+        if (c != '\r') {
+            return token(KSharpTokenType.NewLine, 1)
+        }
+    }
+}
+
+fun Lexer.whiteSpace(): LexerToken {
+    while (true) {
+        val c = this.nextChar() ?: return token(KSharpTokenType.WhiteSpace, 1)
+        if (!c.isWhitespace()) {
+            return token(KSharpTokenType.WhiteSpace, 1)
+        }
+    }
+}
+
+
+val kSharpTokenFactory: TokenFactory = { c ->
+    with(c) {
+        when {
+            isLetter() ->
+                word(
+                    if (isUpperCase()) KSharpTokenType.UpperCaseWord
+                    else KSharpTokenType.LowerCaseWord
+                )
+
+            isDigit() -> number()
+            isDot() -> decimal(KSharpTokenType.Operator, 1)
+            isNewLine() -> newLine()
+            isWhitespace() -> whiteSpace()
+            isOperator() -> operator().also { println(it) }
+            else -> mappings[c]?.let {
+                token(it, 0)
+            }
+        }
+    }
+}
