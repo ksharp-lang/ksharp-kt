4a32c4eaf973e2e1a49433a30a67d7fd599576e4
fernandojerez <fernandojerez@gmail.com>
2022-12-07 07:36:17 -0500

    add module to contains all the nodes

---
+++ nodes/build.gradle.kts
@@ -0,0 +1,14 @@
+plugins {
+    kotlin("jvm")
+}
+
+repositories {
+    mavenCentral()
+}
+
+dependencies {
+    implementation(kotlin("stdlib"))
+    implementation(project(":common"))
+    implementation(project(":test"))
+    testImplementation("io.kotest:kotest-runner-junit5:5.5.4")
+}
