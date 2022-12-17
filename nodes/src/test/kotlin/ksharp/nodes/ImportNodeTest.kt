ee4ce917636b6f32bf00add5d46b8334f2d03672
fernandojerez <fernandojerez@gmail.com>
2022-12-16 19:31:51 -0500

    fix tests

--- nodes/src/test/kotlin/ksharp/nodes/ImportNodeTest.kt
+++ nodes/src/test/kotlin/ksharp/nodes/ImportNodeTest.kt
@@ -3,15 +3,17 @@ package ksharp.nodes
 import io.kotest.core.spec.style.StringSpec
 import io.kotest.matchers.sequences.shouldBeEmpty
 import io.kotest.matchers.shouldBe
+import org.ksharp.common.Location
 
 class ImportNodeTest : StringSpec({
     "Test Node Interface over ModuleNode" {
-        ImportNode("ksharp.math", "m")
+        ImportNode("ksharp.math", "m", Location.NoProvided)
             .node.apply {
                 children.shouldBeEmpty()
                 cast<ImportNode>().apply {
                     moduleName.shouldBe("ksharp.math")
                     key.shouldBe("m")
+                    location.shouldBe(Location.NoProvided)
                 }
             }
     }
--- nodes/src/test/kotlin/ksharp/nodes/ModuleNodeTest.kt
+++ nodes/src/test/kotlin/ksharp/nodes/ModuleNodeTest.kt
@@ -3,27 +3,29 @@ package ksharp.nodes
 import io.kotest.core.spec.style.StringSpec
 import io.kotest.matchers.nulls.shouldBeNull
 import io.kotest.matchers.shouldBe
+import org.ksharp.common.Line
 import org.ksharp.common.Location
+import org.ksharp.common.Offset
 
 private val location = Location(
     context = "file.ks",
-    start = 0 to 0,
-    end = 0 to 0
+    position = Line(0) to Offset(0)
 )
 
 class ModuleNodeTest : StringSpec({
     "Test Node Interface over ModuleNode" {
         ModuleNode(
             name = "ksharp.math",
-            imports = listOf(ImportNode("ksharp.num", "n"))
+            imports = listOf(ImportNode("ksharp.num", "n", location)),
+            location
         ).node.apply {
             cast<ModuleNode>().apply {
                 name.shouldBe("ksharp.math")
-                imports.shouldBe(listOf(ImportNode("ksharp.num", "n")))
+                imports.shouldBe(listOf(ImportNode("ksharp.num", "n", location)))
             }
             parent.shouldBeNull()
             children.toList().shouldBe(
-                listOf(Node(this, ImportNode("ksharp.num", "n")))
+                listOf(Node(this, location, ImportNode("ksharp.num", "n", location)))
             )
         }
     }
