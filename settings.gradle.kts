654491491053a4684d7c79a95ba62bb3dbbc2624
fernandojerez <fernandojerez@gmail.com>
2022-12-16 20:31:08 -0500

    add sonarqube feature

--- settings.gradle.kts
+++ settings.gradle.kts
@@ -1,4 +1,3 @@
-
 rootProject.name = "ksharp-kt"
 
 pluginManagement {
@@ -6,9 +5,11 @@ pluginManagement {
         kotlin("jvm") version "1.7.21" apply false
         kotlin("kapt") version "1.7.21" apply false
         kotlin("plugin.serialization") version "1.7.21" apply false
+        id("org.sonarqube") version "3.5.0.2730"
         jacoco
     }
 }
+
 include("typesystem")
 include("common")
 include("test")
