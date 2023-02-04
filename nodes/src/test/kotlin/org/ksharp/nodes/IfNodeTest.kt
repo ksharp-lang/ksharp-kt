d0606a9f5a6c780b4c8937cdf63a5c409cd3cf9e
fernandojerez <fernandojerez@gmail.com>
2023-02-03 21:35:17 -0500

    add if node

---
+++ nodes/src/test/kotlin/org/ksharp/nodes/IfNodeTest.kt
@@ -0,0 +1,44 @@
+package org.ksharp.nodes
+
+import io.kotest.core.spec.style.StringSpec
+import io.kotest.matchers.nulls.shouldBeNull
+import io.kotest.matchers.shouldBe
+import org.ksharp.common.Location
+
+class IfNodeTest : StringSpec({
+    "Test Node Interface over IfNode" {
+        IfNode(
+            LiteralValueNode("1", LiteralValueType.Integer, Location.NoProvided),
+            LiteralValueNode("2", LiteralValueType.Integer, Location.NoProvided),
+            LiteralValueNode("3", LiteralValueType.Integer, Location.NoProvided),
+            Location.NoProvided
+        ).node.apply {
+            cast<IfNode>().apply {
+                condition.shouldBe(LiteralValueNode("1", LiteralValueType.Integer, Location.NoProvided))
+                trueExpression.shouldBe(LiteralValueNode("2", LiteralValueType.Integer, Location.NoProvided))
+                falseExpression.shouldBe(LiteralValueNode("3", LiteralValueType.Integer, Location.NoProvided))
+                location.shouldBe(Location.NoProvided)
+            }
+            parent.shouldBeNull()
+            children.toList().shouldBe(
+                listOf(
+                    Node(
+                        this,
+                        Location.NoProvided,
+                        LiteralValueNode("1", LiteralValueType.Integer, Location.NoProvided)
+                    ),
+                    Node(
+                        this,
+                        Location.NoProvided,
+                        LiteralValueNode("2", LiteralValueType.Integer, Location.NoProvided)
+                    ),
+                    Node(
+                        this,
+                        Location.NoProvided,
+                        LiteralValueNode("3", LiteralValueType.Integer, Location.NoProvided)
+                    )
+                )
+            )
+        }
+    }
+})
