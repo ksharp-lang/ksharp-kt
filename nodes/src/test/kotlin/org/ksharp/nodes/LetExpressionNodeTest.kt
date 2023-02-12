1d16d2973121e14db842fd6cce22eef0e49fa6ce
fernandojerez <fernandojerez@gmail.com>
2023-02-12 08:32:46 -0500

    add match assignment and let

---
+++ nodes/src/test/kotlin/org/ksharp/nodes/LetExpressionNodeTest.kt
@@ -0,0 +1,65 @@
+package org.ksharp.nodes
+
+import io.kotest.core.spec.style.StringSpec
+import io.kotest.matchers.nulls.shouldBeNull
+import io.kotest.matchers.shouldBe
+import org.ksharp.common.Location
+
+class LetExpressionNodeTest : StringSpec({
+    "Test Node Interface over LetExpressionNode" {
+        LetExpressionNode(
+            listOf(
+                MatchAssignNode(
+                    MatchValueNode(
+                        MatchValueType.Expression,
+                        UnitNode(Location.NoProvided),
+                        Location.NoProvided
+                    ),
+                    LiteralValueNode("10", LiteralValueType.Integer, Location.NoProvided),
+                    Location.NoProvided
+                )
+            ),
+            LiteralValueNode("5", LiteralValueType.Integer, Location.NoProvided),
+            Location.NoProvided
+        ).node.apply {
+            cast<LetExpressionNode>().apply {
+                matches.shouldBe(
+                    listOf(
+                        MatchAssignNode(
+                            MatchValueNode(
+                                MatchValueType.Expression,
+                                UnitNode(Location.NoProvided),
+                                Location.NoProvided
+                            ),
+                            LiteralValueNode("10", LiteralValueType.Integer, Location.NoProvided),
+                            Location.NoProvided
+                        )
+                    )
+                )
+                expression.shouldBe(LiteralValueNode("5", LiteralValueType.Integer, Location.NoProvided))
+                location.shouldBe(Location.NoProvided)
+            }
+            parent.shouldBeNull()
+            children.toList().shouldBe(
+                listOf(
+                    Node(
+                        this, Location.NoProvided, MatchAssignNode(
+                            MatchValueNode(
+                                MatchValueType.Expression,
+                                UnitNode(Location.NoProvided),
+                                Location.NoProvided
+                            ),
+                            LiteralValueNode("10", LiteralValueType.Integer, Location.NoProvided),
+                            Location.NoProvided
+                        )
+                    ),
+                    Node(
+                        this,
+                        Location.NoProvided,
+                        LiteralValueNode("5", LiteralValueType.Integer, Location.NoProvided)
+                    )
+                )
+            )
+        }
+    }
+})
