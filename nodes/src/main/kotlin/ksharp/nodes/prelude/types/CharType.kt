bcb70531c2d3d4ad3348473e3b78be0f24a9f0bb
Fernando Jerez <fernandojerez@gmail.com>
2023-02-19 19:04:08 -0500

    10 implement prelude (#19)
    * add Intepreted Type for interactive mode
    
    * add prelude types

---
+++ nodes/src/main/kotlin/ksharp/nodes/prelude/types/CharType.kt
@@ -0,0 +1,11 @@
+package ksharp.nodes.prelude.types
+
+import org.ksharp.typesystem.types.Type
+
+class CharType : Type {
+    override val compound: Boolean = false
+    override val terms: Sequence<Type> = emptySequence()
+    override val representation: String = "char<Char>"
+}
+
+val charType = CharType()
---
+++ nodes/src/main/kotlin/ksharp/nodes/prelude/types/PreludeTypeSystem.kt
@@ -0,0 +1,49 @@
+package ksharp.nodes.prelude.types
+
+import org.ksharp.common.Either
+import org.ksharp.typesystem.TypeSystemBuilder
+import org.ksharp.typesystem.typeSystem
+import org.ksharp.typesystem.types.alias
+import org.ksharp.typesystem.types.parametricType
+
+private fun TypeSystemBuilder.number(alias: String, type: Numeric) =
+    alias(alias) {
+        parametricType("Num") {
+            numeric(type)
+        }
+    }
+
+private fun createPreludeTypeSystem() = typeSystem {
+    alias("Char") { Either.Right(charType) }
+    parametricType("Num") {
+        parameter("a")
+    }
+
+    number("Byte", Numeric.Byte)
+    number("Short", Numeric.Short)
+    number("Int", Numeric.Int)
+    number("Long", Numeric.Long)
+    number("BigInt", Numeric.BigInt)
+    number("Float", Numeric.Float)
+    number("Double", Numeric.Double)
+    number("BigDecimal", Numeric.BigDecimal)
+
+    parametricType("List") {
+        parameter("v")
+    }
+    parametricType("Set") {
+        parameter("v")
+    }
+    parametricType("Map") {
+        parameter("k")
+        parameter("v")
+    }
+
+    alias("String") {
+        parametricType("List") {
+            type("Char")
+        }
+    }
+}
+
+val preludeTypeSystem = createPreludeTypeSystem()
