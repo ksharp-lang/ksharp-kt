13bbc1b08570a44835452338d881d220d3454485
fernandojerez <fernandojerez@gmail.com>
2023-02-20 20:28:50 -0500

    change containsKey

--- typesystem/src/main/kotlin/org/ksharp/typesystem/TypeSystemBuilder.kt
+++ typesystem/src/main/kotlin/org/ksharp/typesystem/TypeSystemBuilder.kt
@@ -37,7 +37,9 @@ class TypeSystemBuilder(
             store.get(key) ?: parent?.get(key)?.valueOrNull
 
         override fun containsKey(key: String): Boolean =
-            store.containsKey(key) == true || (parent?.get(key)?.valueOrNull != null)
+            if (parent != null)
+                store.containsKey(key) == true || (parent[key].valueOrNull != null)
+            else store.containsKey(key) == true
 
     }
 
