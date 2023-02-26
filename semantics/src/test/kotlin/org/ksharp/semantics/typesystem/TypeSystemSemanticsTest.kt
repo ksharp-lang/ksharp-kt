bb9dcbd22fe37939b1b0b5045defce1e2efefba5
fernandojerez <fernandojerez@gmail.com>
2023-02-26 11:43:03 -0500

    check type already defined

--- semantics/src/test/kotlin/org/ksharp/semantics/typesystem/TypeSystemSemanticsTest.kt
+++ semantics/src/test/kotlin/org/ksharp/semantics/typesystem/TypeSystemSemanticsTest.kt
@@ -4,10 +4,13 @@ import io.kotest.core.spec.style.StringSpec
 import io.kotest.matchers.booleans.shouldBeFalse
 import io.kotest.matchers.booleans.shouldBeTrue
 import io.kotest.matchers.collections.shouldBeEmpty
+import io.kotest.matchers.shouldBe
 import org.ksharp.common.Location
+import org.ksharp.common.new
 import org.ksharp.nodes.ConcreteTypeNode
 import org.ksharp.nodes.ModuleNode
 import org.ksharp.nodes.TypeNode
+import org.ksharp.semantics.scopes.TableErrorCode
 import org.ksharp.test.shouldBeRight
 
 class TypeSystemSemanticsTest : StringSpec({
@@ -38,7 +41,7 @@ class TypeSystemSemanticsTest : StringSpec({
             typeSystem["Integer"].map { it.representation }.shouldBeRight("Int")
         }
     }
-    "Type already defined" {
+    "check Type already defined" {
         ModuleNode(
             "module",
             listOf(),
@@ -49,19 +52,29 @@ class TypeSystemSemanticsTest : StringSpec({
                     listOf(),
                     ConcreteTypeNode("Int", Location.NoProvided),
                     Location.NoProvided
+                ),
+                TypeNode(
+                    true,
+                    "Integer",
+                    listOf(),
+                    ConcreteTypeNode("Long", Location.NoProvided),
+                    Location.NoProvided
                 )
             ),
             listOf(),
             listOf(),
             Location.NoProvided
         ).checkSemantics().apply {
-            errors.shouldBeEmpty()
+            errors.shouldBe(
+                listOf(
+                    TableErrorCode.AlreadyDefined.new(Location.NoProvided, "classifier" to "Type", "name" to "Integer")
+                )
+            )
             typeSystemTable["Integer"]!!
                 .apply {
                     isInternal.shouldBeFalse()
                     isPublic.shouldBeTrue()
                 }
-            typeSystem["Int"].map { it.representation }.shouldBeRight("(Num numeric<Int>)")
             typeSystem["Integer"].map { it.representation }.shouldBeRight("Int")
         }
     }
