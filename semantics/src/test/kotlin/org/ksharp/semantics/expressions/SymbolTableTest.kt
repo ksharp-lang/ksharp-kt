efd95f9b1d737a07a3021134194c4626e8176347
fernandojerez <fernandojerez@gmail.com>
2023-03-06 20:32:09 -0500

    add symbol tests

--- semantics/src/test/kotlin/org/ksharp/semantics/expressions/SymbolTableTest.kt
+++ semantics/src/test/kotlin/org/ksharp/semantics/expressions/SymbolTableTest.kt
@@ -1,6 +1,7 @@
 package org.ksharp.semantics.expressions
 
 import io.kotest.core.spec.style.StringSpec
+import io.kotest.matchers.nulls.shouldBeNull
 import io.kotest.matchers.shouldBe
 import org.ksharp.common.Location
 import org.ksharp.common.new
@@ -9,6 +10,9 @@ import org.ksharp.semantics.prelude.types.preludeTypeSystem
 import org.ksharp.semantics.scopes.TableErrorCode
 import org.ksharp.test.shouldBeLeft
 import org.ksharp.test.shouldBeRight
+import org.ksharp.typesystem.typeSystem
+import org.ksharp.typesystem.types.alias
+import org.ksharp.typesystem.types.parametricType
 
 class SymbolTableTest : StringSpec({
     "Create symbol table" {
@@ -51,7 +55,7 @@ class SymbolTableTest : StringSpec({
             this["c"]!!.apply {
                 first.shouldBe(
                     Symbol(
-                        typeSystem["String"].valueOrNull!!,
+                        typeSystem["String"].valueOrNull!!.also { println(it) },
                         3,
                         RecordSize.Single
                     )
@@ -105,6 +109,45 @@ class SymbolTableTest : StringSpec({
                     )
                     second.shouldBe(Location.NoProvided)
                 }
+            }
+        }
+    }
+    "Accessing no registered symbol" {
+        val typeSystem = preludeTypeSystem.value
+        SymbolTableBuilder(null, ErrorCollector()).apply {
+            register(
+                "a", typeSystem["Int"], Location.NoProvided
+            ).shouldBeRight()
+        }.build().apply {
+            summary.shouldBe(SymbolSummary(1))
+            this["c"].shouldBeNull()
+        }
+    }
+    "Symbol table with compound types" {
+        val typeSystem = typeSystem(preludeTypeSystem) {
+            alias("KeyMap") {
+                parametricType("Map") {
+                    type("String")
+                    type("String")
+                }
+            }
+        }.value
+
+        SymbolTableBuilder(null, ErrorCollector()).apply {
+            register(
+                "a", typeSystem["KeyMap"], Location.NoProvided
+            ).shouldBeRight()
+        }.build().apply {
+            summary.shouldBe(SymbolSummary(1))
+            this["a"]!!.apply {
+                first.shouldBe(
+                    Symbol(
+                        typeSystem["KeyMap"].valueOrNull!!,
+                        0,
+                        RecordSize.Single
+                    )
+                )
+                second.shouldBe(Location.NoProvided)
             }
         }
     }
