c132af4ae946f2a371a17546df561ba52667797a
fernandojerez <fernandojerez@gmail.com>
2023-03-08 19:43:21 -0500

    make MaybePolymorphicPromiseType a data class

--- semantics/src/main/kotlin/org/ksharp/semantics/inference/TypePromise.kt
+++ semantics/src/main/kotlin/org/ksharp/semantics/inference/TypePromise.kt
@@ -1,6 +1,5 @@
 package org.ksharp.semantics.inference
 
-import org.ksharp.typesystem.types.Parameter
 import org.ksharp.typesystem.types.Type
 
 interface TypePromise
@@ -9,33 +8,7 @@ data class ResolvedTypePromise(
     val type: Type
 ) : TypePromise
 
-class MaybePolymorphicTypePromise(
+data class MaybePolymorphicTypePromise(
     val name: String,
-    paramName: String,
-) : TypePromise {
-    var type: Type = Parameter(paramName)
-        private set
-
-    override fun equals(other: Any?): Boolean {
-        if (this === other) return true
-        if (javaClass != other?.javaClass) return false
-
-        other as MaybePolymorphicTypePromise
-
-        if (name != other.name) return false
-        if (type != other.type) return false
-
-        return true
-    }
-
-    override fun hashCode(): Int {
-        var result = name.hashCode()
-        result = 31 * result + type.hashCode()
-        return result
-    }
-
-    override fun toString(): String {
-        return "MaybePolymorphicTypePromise(name='$name', type=$type)"
-    }
-
-}
+    val paramName: String,
+) : TypePromise
