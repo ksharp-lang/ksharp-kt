eb0b1da336ae2276892e8755ba14ea0b355955f5
fernandojerez <fernandojerez@gmail.com>
2023-04-08 23:12:47 -0500

    clean code

--- common/src/main/kotlin/org/ksharp/common/io/Buffer.kt
+++ common/src/main/kotlin/org/ksharp/common/io/Buffer.kt
@@ -1,15 +1,5 @@
 package org.ksharp.common.io
 
-import io.netty.buffer.ByteBuf
-import io.netty.buffer.PooledByteBufAllocator
-import java.io.OutputStream
-
-private val allocator = PooledByteBufAllocator.DEFAULT
-
-
-/**
- * Calling methods after the buffer is written produce an exception
- */
 interface BufferWriter {
     val size: Int
     fun add(value: String): Int
