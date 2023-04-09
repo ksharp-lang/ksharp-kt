f3a38a732b7d24a24900c21e78080a430622cf9f
fernandojerez <fernandojerez@gmail.com>
2023-04-09 08:56:12 -0500

    add buffer test

---
+++ common/src/test/kotlin/org/ksharp/common/io/BufferTest.kt
@@ -0,0 +1,31 @@
+package org.ksharp.common.io
+
+import io.kotest.core.spec.style.StringSpec
+import io.kotest.matchers.nulls.shouldNotBeNull
+import io.kotest.matchers.shouldBe
+import java.io.ByteArrayInputStream
+import java.io.ByteArrayOutputStream
+
+class BufferTest : StringSpec({
+    "Test buffer writer and buffer view" {
+        val output = ByteArrayOutputStream()
+        newBufferWriter().apply {
+            add(1)
+            add(5)
+            add("Hello")
+            add(6)
+            set(0, 78)
+            size.shouldBe(17)
+            transferTo(output)
+        }
+        val bytes = output.toByteArray()
+        bytes.size.shouldBe(17)
+        ByteArrayInputStream(bytes).bufferView {
+            it.readInt(0).shouldBe(78)
+            it.readInt(4).shouldBe(5)
+            it.readString(8, 5).shouldBe("Hello")
+            it.readInt(13).shouldBe(6)
+        }.shouldNotBeNull()
+
+    }
+})
