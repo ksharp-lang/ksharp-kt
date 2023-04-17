99d02891ef90ae366ed738346fe1a104da27f235
fernandojerez <fernandojerez@gmail.com>
2023-04-16 22:57:26 -0500

    ad dmethod to convert list of semantic info into a list of types

--- semantics/src/main/kotlin/org/ksharp/semantics/nodes/SemanticInfo.kt
+++ semantics/src/main/kotlin/org/ksharp/semantics/nodes/SemanticInfo.kt
@@ -1,12 +1,19 @@
 package org.ksharp.semantics.nodes
 
+import org.ksharp.common.Either
+import org.ksharp.common.add
+import org.ksharp.common.isLeft
+import org.ksharp.common.listBuilder
 import org.ksharp.semantics.inference.TypePromise
 import org.ksharp.semantics.inference.getTypePromise
+import org.ksharp.semantics.inference.type
 import org.ksharp.semantics.scopes.SymbolTable
 import org.ksharp.semantics.scopes.SymbolTableBuilder
 import org.ksharp.semantics.scopes.Table
 import org.ksharp.semantics.scopes.TableValue
+import org.ksharp.typesystem.ErrorOrType
 import org.ksharp.typesystem.TypeSystem
+import org.ksharp.typesystem.types.Type
 
 
 sealed class SemanticInfo
@@ -44,3 +51,21 @@ data class TypeSemanticInfo(
 
 fun TypeSystem.getTypeSemanticInfo(name: String) =
     TypeSemanticInfo(getTypePromise(name))
+
+val SemanticInfo.type: ErrorOrType
+    get() =
+        when (this) {
+            is TypeSemanticInfo -> type.type
+            else -> TODO()
+        }
+
+val List<SemanticInfo>.types
+    get() = run {
+        val builder = listBuilder<Type>()
+        for (info in this) {
+            val type = info.type
+            if (type.isLeft) return@run type
+            else builder.add(type.valueOrNull!!)
+        }
+        Either.Right(builder.build())
+    }
