166bc8246e1a7fbc412ebf12baeae85a88aea09e
Heli Jerez <fernandojerez@gmail.com>
2023-05-18 21:11:19 -0500

    add tests for Path and File

--- compiler/src/test/kotlin/org/ksharp/compiler/CompilerTest.kt
+++ compiler/src/test/kotlin/org/ksharp/compiler/CompilerTest.kt
@@ -12,6 +12,8 @@ import org.ksharp.nodes.semantic.ConstantNode
 import org.ksharp.semantics.nodes.AbstractionSemanticInfo
 import org.ksharp.semantics.nodes.TypeSemanticInfo
 import org.ksharp.test.shouldBeRight
+import java.io.File
+import kotlin.io.path.Path
 
 class CompilerTestModuleInfo : StringSpec({
     "Create a moduleinfo from a String" {
@@ -76,6 +78,72 @@ class CompilerTestModuleInfo : StringSpec({
                                 ),
                                 location = Location(
                                     context = "file1",
+                                    position = Line(value = 1) to Offset(value = 0)
+                                )
+                            )
+                        )
+                    )
+                }
+            }
+    }
+    "Create a moduleinfo from a File" {
+        File("src/test/resources/ten.ff")
+            .moduleInfo()
+            .shouldBeRight()
+            .apply {
+                map {
+                    it.name.shouldBe("ten")
+                    it.errors.shouldBeEmpty()
+                    it.abstractions.shouldBe(
+                        listOf(
+                            AbstractionNode(
+                                name = "ten",
+                                expression = ConstantNode(
+                                    value = 10.toLong(),
+                                    info = TypeSemanticInfo(type = it.typeSystem.get("Byte")),
+                                    location = Location(
+                                        context = "ten.ff", position = Line(value = 1) to Offset(value = 0)
+                                    )
+                                ),
+                                info = AbstractionSemanticInfo(
+                                    visibility = FunctionVisibility.Internal,
+                                    parameters = listOf()
+                                ),
+                                location = Location(
+                                    context = "ten.ff",
+                                    position = Line(value = 1) to Offset(value = 0)
+                                )
+                            )
+                        )
+                    )
+                }
+            }
+    }
+    "Create a moduleinfo from a Path" {
+        Path("src/test/resources/ten.ff")
+            .moduleInfo()
+            .shouldBeRight()
+            .apply {
+                map {
+                    it.name.shouldBe("ten")
+                    it.errors.shouldBeEmpty()
+                    it.abstractions.shouldBe(
+                        listOf(
+                            AbstractionNode(
+                                name = "ten",
+                                expression = ConstantNode(
+                                    value = 10.toLong(),
+                                    info = TypeSemanticInfo(type = it.typeSystem.get("Byte")),
+                                    location = Location(
+                                        context = "ten.ff", position = Line(value = 1) to Offset(value = 0)
+                                    )
+                                ),
+                                info = AbstractionSemanticInfo(
+                                    visibility = FunctionVisibility.Internal,
+                                    parameters = listOf()
+                                ),
+                                location = Location(
+                                    context = "ten.ff",
                                     position = Line(value = 1) to Offset(value = 0)
                                 )
                             )
