abedd2fad71a73b917ac7ac23293ac997ea09e6f
Heli Jerez <fernandojerez@gmail.com>
2023-06-01 22:52:12 -0500

    remove rgs

--- lsp/src/main/kotlin/org/ksharp/lsp/Main.kt
+++ lsp/src/main/kotlin/org/ksharp/lsp/Main.kt
@@ -5,7 +5,7 @@ package org.ksharp.lsp
 import org.eclipse.lsp4j.launch.LSPLauncher
 
 
-fun main(args: Array<String>) {
+fun main() {
     val lsp = KSharpLanguageServer()
     val launcher = LSPLauncher.createServerLauncher(lsp, System.`in`, System.out)
     val client = launcher.remoteProxy
