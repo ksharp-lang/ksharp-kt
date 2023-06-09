72254b6a0415cc0ef96e9e0e55c5678ec6eca464
Heli Jerez <fernandojerez@gmail.com>
2023-06-09 08:00:19 -0500

    Add semantic tokens tests

--- lsp/src/test/kotlin/org/ksharp/lsp/capabilities/semantic_tokens/CalculateSemanticTokensTest.kt
+++ lsp/src/test/kotlin/org/ksharp/lsp/capabilities/semantic_tokens/CalculateSemanticTokensTest.kt
@@ -1,70 +1,39 @@
 package org.ksharp.lsp.capabilities.semantic_tokens
 
-import io.kotest.core.spec.style.StringSpec
+import io.kotest.core.spec.style.FreeSpec
 import io.kotest.matchers.shouldBe
 
-class CalculateSemanticTokensTest : StringSpec({
-    "invalid tokens" {
-        calculateSemanticTokens("1 + 2")
-            .shouldBe(listOf(0, 0, 1, 5, 0, 0, 2, 1, 4, 0, 0, 2, 1, 5, 0))
-    }
-    "parametric type token" {
-        calculateSemanticTokens("type Map a b = Map a b")
-            .shouldBe(
-                listOf(
-                    0,
-                    0,
-                    4,
-                    6,
-                    0,
-                    0,
-                    5,
-                    3,
-                    0,
-                    1,
-                    0,
-                    4,
-                    1,
-                    9,
-                    0,
-                    0,
-                    2,
-                    1,
-                    9,
-                    0,
-                    0,
-                    2,
-                    1,
-                    4,
-                    0,
-                    0,
-                    2,
-                    3,
-                    3,
-                    0,
-                    0,
-                    4,
-                    1,
-                    10,
-                    0,
-                    0,
-                    2,
-                    1,
-                    10,
-                    0
-                )
-            )
-    }
-    "union type" {
-        calculateSemanticTokens("type Bool = True | False")
-            .shouldBe(listOf(0, 0, 4, 6, 0, 0, 5, 4, 0, 1, 0, 5, 1, 4, 0, 0, 2, 4, 3, 0, 0, 5, 1, 4, 0, 0, 2, 5, 3, 0))
-    }
-    "intersection type" {
-        calculateSemanticTokens("type Bool = True & False")
-            .shouldBe(listOf(0, 0, 4, 6, 0, 0, 5, 4, 0, 1, 0, 5, 1, 4, 0, 0, 2, 4, 3, 0, 0, 5, 1, 4, 0, 0, 2, 5, 3, 0))
-    }
-    "tuple type" {
-        calculateSemanticTokens("type Bool = True, False")
-            .shouldBe(listOf(0, 0, 4, 6, 0, 0, 5, 4, 0, 1, 0, 5, 1, 4, 0, 0, 2, 4, 3, 0, 0, 4, 1, 4, 0, 0, 2, 5, 3, 0))
+private fun String.spec(document: String, expectedTokens: String) =
+    this to (document to
+            expectedTokens.splitToSequence(",")
+                .map { it.trim().toInt() }.toList())
+
+private val specs = listOf(
+    "Invalid tokens".spec("1 + 2", "0, 0, 1, 5, 0, 0, 2, 1, 4, 0, 0, 2, 1, 5, 0"),
+    "Parametric tokens".spec(
+        "type Map a b = Map a b",
+        "0, 0, 4, 6, 0, 0, 5, 3, 0, 1, 0, 4, 1, 9, 0, 0, 2, 1, 9, 0, 0, 2, 1, 4, 0, 0, 2, 3, 3, 0, 0, 4, 1, 10, 0, 0, 2, 1, 10, 0"
+    ),
+    "Union types".spec(
+        "type Bool = True | False",
+        "0, 0, 4, 6, 0, 0, 5, 4, 0, 1, 0, 5, 1, 4, 0, 0, 2, 4, 3, 0, 0, 5, 1, 4, 0, 0, 2, 5, 3, 0"
+    ),
+    "Intersected types".spec(
+        "type Bool = True & False",
+        "0, 0, 4, 6, 0, 0, 5, 4, 0, 1, 0, 5, 1, 4, 0, 0, 2, 4, 3, 0, 0, 5, 1, 4, 0, 0, 2, 5, 3, 0"
+    ),
+    "Tuple types".spec(
+        "type Point = x: Int, y: Int",
+        "0, 0, 4, 6, 0, 0, 5, 5, 0, 1, 0, 6, 1, 4, 0, 0, 2, 2, 11, 0, 0, 3, 3, 3, 0, 0, 3, 1, 4, 0, 0, 2, 2, 11, 0, 0, 3, 3, 3, 0"
+    )
+)
+
+class CalculateSemanticTokensTest : FreeSpec({
+    "Calculate semantic tokens " - {
+        specs.forEach { (desc, spec) ->
+            desc {
+                calculateSemanticTokens(spec.first).shouldBe(spec.second)
+            }
+        }
     }
 })
