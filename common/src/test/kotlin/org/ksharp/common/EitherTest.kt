06ee36b3a827d87c65f9b6d81aed95a31b1c3aba
Heli Jerez <fernandojerez@gmail.com>
2023-06-10 12:59:49 -0500

    Add more tests

--- common/src/test/kotlin/org/ksharp/common/EitherTest.kt
+++ common/src/test/kotlin/org/ksharp/common/EitherTest.kt
@@ -35,6 +35,18 @@ class EitherTest : StringSpec({
                 isRight.shouldBe(true)
             }
     }
+    "flat map left on a right Either" {
+        (Either.Right("World")
+                as Either<String, String>)
+            .flatMapLeft {
+                Either.Right(it.length)
+            }
+            .apply {
+                shouldBe(Either.Right("World"))
+                isLeft.shouldBe(false)
+                isRight.shouldBe(true)
+            }
+    }
     "flat map a left Either 2" {
         (Either.Left("Hello")
                 as Either<String, String>)
