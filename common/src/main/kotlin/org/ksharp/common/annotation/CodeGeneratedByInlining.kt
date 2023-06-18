9f64a37c55b3af37663b7270e91415ba043f8050
fernandojerez <fernandojerez@gmail.com>
2023-06-18 16:42:39 -0500

    remove ()

--- common/src/main/kotlin/org/ksharp/common/annotation/CodeGeneratedByInlining.kt
+++ common/src/main/kotlin/org/ksharp/common/annotation/CodeGeneratedByInlining.kt
@@ -2,4 +2,4 @@ package org.ksharp.common.annotation
 
 @Retention(AnnotationRetention.BINARY)
 @Target(AnnotationTarget.FUNCTION)
-annotation class CodeGeneratedByInlining()
+annotation class CodeGeneratedByInlining
