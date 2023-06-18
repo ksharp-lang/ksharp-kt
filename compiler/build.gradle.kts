c7a759e6a77aac86379ffa8a7a8cb418bb89dcb0
fernandojerez <fernandojerez@gmail.com>
2023-06-18 16:54:36 -0500

    update dependencies

--- compiler/build.gradle.kts
+++ compiler/build.gradle.kts
@@ -18,7 +18,7 @@ dependencies {
     implementation(project(":semantics"))
     implementation(project(":typesystem"))
     testImplementation(project(":test"))
-    testImplementation("io.kotest:kotest-runner-junit5:5.5.5")
+    testImplementation("io.kotest:kotest-runner-junit5:5.6.2")
 }
 
 tasks.test {
--- module/build.gradle.kts
+++ module/build.gradle.kts
@@ -12,6 +12,6 @@ dependencies {
     implementation(project(":typesystem"))
     implementation(project(":nodes"))
     implementation(project(":test"))
-    implementation("io.netty:netty-buffer:4.1.91.Final")
-    testImplementation("io.kotest:kotest-runner-junit5:5.5.5")
+    implementation("io.netty:netty-buffer:4.1.93.Final")
+    testImplementation("io.kotest:kotest-runner-junit5:5.6.2")
 }
