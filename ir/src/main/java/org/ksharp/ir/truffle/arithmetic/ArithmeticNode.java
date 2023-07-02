ec73997e68548be422af171968ed8effcc108416
fernandojerez <fernandojerez@gmail.com>
2023-07-01 19:33:30 -0500

    removed unused code

--- ir/src/main/java/org/ksharp/ir/truffle/arithmetic/ArithmeticNode.java
+++ ir/src/main/java/org/ksharp/ir/truffle/arithmetic/ArithmeticNode.java
@@ -2,10 +2,7 @@ package org.ksharp.ir.truffle.arithmetic;
 
 import com.oracle.truffle.api.dsl.NodeField;
 import com.oracle.truffle.api.dsl.Specialization;
-import com.oracle.truffle.api.frame.VirtualFrame;
-import com.oracle.truffle.api.nodes.UnexpectedResultException;
 import org.ksharp.ir.truffle.BinaryOperationNode;
-import org.ksharp.ir.truffle.PreludeTypeSystemGen;
 
 import java.math.BigDecimal;
 import java.math.BigInteger;
@@ -53,38 +50,6 @@ public abstract class ArithmeticNode extends BinaryOperationNode {
     @Specialization
     protected BigDecimal doBigDecimal(BigDecimal left, BigDecimal right) {
         return getOperations().doBigDecimal(left, right);
-    }
-
-    public byte executeByte(VirtualFrame frame) throws UnexpectedResultException {
-        return PreludeTypeSystemGen.expectByte(execute(frame));
-    }
-
-    public short executeShort(VirtualFrame frame) throws UnexpectedResultException {
-        return PreludeTypeSystemGen.expectShort(execute(frame));
-    }
-
-    public int executeInt(VirtualFrame frame) throws UnexpectedResultException {
-        return PreludeTypeSystemGen.expectInteger(execute(frame));
-    }
-
-    public long executeLong(VirtualFrame frame) throws UnexpectedResultException {
-        return PreludeTypeSystemGen.expectLong(execute(frame));
-    }
-
-    public float executeFloat(VirtualFrame frame) throws UnexpectedResultException {
-        return PreludeTypeSystemGen.expectFloat(execute(frame));
-    }
-
-    public double executeDouble(VirtualFrame frame) throws UnexpectedResultException {
-        return PreludeTypeSystemGen.expectDouble(execute(frame));
-    }
-
-    public BigInteger executeBigInteger(VirtualFrame frame) throws UnexpectedResultException {
-        return PreludeTypeSystemGen.expectBigInteger(execute(frame));
-    }
-
-    public BigDecimal executeBigDecimal(VirtualFrame frame) throws UnexpectedResultException {
-        return PreludeTypeSystemGen.expectBigDecimal(execute(frame));
     }
 
 }
