b6cbf63bbb394318b4df66baef4c840d7c9f7d39
fernandojerez <fernandojerez@gmail.com>
2023-07-17 07:36:35 -0500

    add iterator test

--- common/src/test/kotlin/org/ksharp/common/IteratorsTest.kt
+++ common/src/test/kotlin/org/ksharp/common/IteratorsTest.kt
@@ -1,5 +1,6 @@
 package org.ksharp.common
 
+import io.kotest.assertions.throwables.shouldThrow
 import io.kotest.core.spec.style.StringSpec
 import io.kotest.matchers.collections.shouldContainAll
 import io.kotest.matchers.shouldBe
@@ -21,5 +22,13 @@ class IteratorsTest : StringSpec({
                 .asSequence()
         ).flatten()
             .toList().shouldBe(listOf(2, 4, 6, 8, 3, 6, 9, 12))
+    }
+    "Test calling next before hasNext" {
+        val iter = listOf(1, 2, 3, 4).iterator()
+        generateIterator { if (iter.hasNext()) iter.next() * 2 else null }.apply {
+            shouldThrow<NoSuchElementException>(){
+                next()
+            }
+        }
     }
 })
