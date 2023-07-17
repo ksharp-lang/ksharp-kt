6074de89c5a221cef31c115cad200ed8bb54c6f8
fernandojerez <fernandojerez@gmail.com>
2023-07-17 07:16:01 -0500

    move to kover

--- settings.gradle.kts
+++ settings.gradle.kts
@@ -3,11 +3,12 @@ rootProject.name = "ksharp-kt"
 pluginManagement {
     val kotlinVersion = "1.9.0"
     val sonarqubeVersion = "4.2.1.3168"
+    val koverVersion = "0.7.2"
 
     plugins {
         kotlin("jvm") version kotlinVersion apply false
         id("org.sonarqube") version sonarqubeVersion
-        jacoco
+        id("org.jetbrains.kotlinx.kover") version koverVersion
     }
 }
 
@@ -20,13 +21,14 @@ dependencyResolutionManagement {
             version("netty", "4.1.94.Final")
 
             // Tooling
-            version("jacoco", "0.8.10")
             version("plugin.com.github.johnrengelman.shadow", "8.1.1")
+            version("plugin.org.jetbrains.kotlinx.kover", "0.7.2")
 
             // Plugins
             plugin("shadow", "com.github.johnrengelman.shadow").versionRef("plugin.com.github.johnrengelman.shadow")
 
             // Libraries
+            library("kover", "org.jetbrains.kotlinx","kover-gradle-plugin").versionRef("plugin.org.jetbrains.kotlinx.kover")
             library("kotest", "io.kotest", "kotest-runner-junit5").versionRef("kotest")
             library("eclipse.lsp4j", "org.eclipse.lsp4j", "org.eclipse.lsp4j").versionRef("eclipse.lsp4j")
             library(
