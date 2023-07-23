7db87cec077131837aaf0979bbefe020ea0ecbad
fernandojerez <fernandojerez@gmail.com>
2023-07-23 13:55:26 -0500

    fix tests

--- module/src/test/kotlin/org/ksharp/module/solver/SolverTest.kt
+++ module/src/test/kotlin/org/ksharp/module/solver/SolverTest.kt
@@ -1,23 +1,23 @@
 package org.ksharp.module.solver
 
 import io.kotest.core.spec.style.StringSpec
-import io.kotest.matchers.shouldBe
 import org.ksharp.module.prelude.preludeTypeSystem
 import org.ksharp.module.prelude.types.Numeric
 import org.ksharp.module.prelude.types.NumericType
 import org.ksharp.module.prelude.types.charType
+import org.ksharp.test.shouldBeRight
 import org.ksharp.typesystem.solver.solve
 
 class SolverTest : StringSpec({
     "char reducer" {
         preludeTypeSystem
             .value
-            .solve(charType).shouldBe(charType)
+            .solve(charType).shouldBeRight(charType)
     }
     "numeric reducer" {
         val intType = NumericType(Numeric.Int)
         preludeTypeSystem
             .value
-            .solve(intType).shouldBe(intType)
+            .solve(intType).shouldBeRight(intType)
     }
 })
