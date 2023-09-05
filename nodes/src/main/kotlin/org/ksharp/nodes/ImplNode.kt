ca78f9114e96bd37f9e7dc5b8b1818b1d880d448
fernandojerez <fernandojerez@gmail.com>
2023-09-04 20:44:28 -0500

    add impl node interface

---
+++ nodes/src/main/kotlin/org/ksharp/nodes/ImplNode.kt
@@ -0,0 +1,22 @@
+package org.ksharp.nodes
+
+import org.ksharp.common.Location
+
+data class ImplNodeLocations(
+    val traitName: Location,
+    val forName: Location,
+    val forKeyword: Location,
+    val assignOperator: Location,
+) : NodeLocations
+
+data class ImplNode(
+    val traitName: String,
+    val forName: String,
+    val functions: List<FunctionNode>,
+    override val location: Location,
+    override val locations: ImplNodeLocations
+) : NodeData() {
+    override val children: Sequence<NodeData>
+        get() = functions.asSequence()
+
+}
