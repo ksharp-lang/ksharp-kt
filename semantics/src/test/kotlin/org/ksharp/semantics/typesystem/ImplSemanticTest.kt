9fe7f1317961cdd9930e986dca60cf60c97f68ef
fernandojerez <fernandojerez@gmail.com>
2023-10-03 21:42:53 -0500

    define impl tests

---
+++ semantics/src/test/kotlin/org/ksharp/semantics/typesystem/ImplSemanticTest.kt
@@ -0,0 +1,12 @@
+package org.ksharp.semantics.typesystem
+
+import io.kotest.core.spec.style.StringSpec
+
+class ImplSemanticTest : StringSpec({
+    "Not allow duplicate Impls" {}
+    "Not allow duplicate methods in Impls" {}
+    "Missing method implementation in Impls" {}
+    "Valid Impl" {}
+    "Impl with missing method, but it has default implementation in trait" {}
+    "Error in method impl" {}
+})
