c1ca9a678bb56417275d2e44abc00d8310a0afb3
fernandojerez <fernandojerez@gmail.com>
2023-10-07 10:57:18 -0500

    update dependencies versions

--- settings.gradle.kts
+++ settings.gradle.kts
@@ -1,9 +1,9 @@
 rootProject.name = "ksharp-kt"
 
 pluginManagement {
-    val kotlinVersion = "1.9.0"
-    val sonarqubeVersion = "4.2.1.3168"
-    val koverVersion = "0.7.2"
+    val kotlinVersion = "1.9.10"
+    val sonarqubeVersion = "4.4.1.3373"
+    val koverVersion = "0.7.3"
 
     plugins {
         kotlin("jvm") version kotlinVersion apply false
@@ -15,20 +15,24 @@ pluginManagement {
 dependencyResolutionManagement {
     versionCatalogs {
         create("libs") {
-            version("kotest", "5.6.2")
-            version("eclipse.lsp4j", "0.21.0")
+            version("kotest", "5.7.2")
+            version("eclipse.lsp4j", "0.21.1")
             version("graalvm", "22.3.0")
-            version("netty", "4.1.94.Final")
+            version("netty", "4.1.99.Final")
 
             // Tooling
             version("plugin.com.github.johnrengelman.shadow", "8.1.1")
-            version("plugin.org.jetbrains.kotlinx.kover", "0.7.2")
+            version("plugin.org.jetbrains.kotlinx.kover", "0.7.3")
 
             // Plugins
             plugin("shadow", "com.github.johnrengelman.shadow").versionRef("plugin.com.github.johnrengelman.shadow")
 
             // Libraries
-            library("kover", "org.jetbrains.kotlinx","kover-gradle-plugin").versionRef("plugin.org.jetbrains.kotlinx.kover")
+            library(
+                "kover",
+                "org.jetbrains.kotlinx",
+                "kover-gradle-plugin"
+            ).versionRef("plugin.org.jetbrains.kotlinx.kover")
             library("kotest", "io.kotest", "kotest-runner-junit5").versionRef("kotest")
             library("eclipse.lsp4j", "org.eclipse.lsp4j", "org.eclipse.lsp4j").versionRef("eclipse.lsp4j")
             library(
