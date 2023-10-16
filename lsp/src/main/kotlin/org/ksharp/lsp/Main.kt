94fe6b57f3ccb6d9a3c2378e66889d27a499dce5
fernandojerez <fernandojerez@gmail.com>
2023-10-16 10:18:15 -0500

    add an annotation to exclude classes from coverage

--- lsp/src/main/kotlin/org/ksharp/lsp/Main.kt
+++ lsp/src/main/kotlin/org/ksharp/lsp/Main.kt
@@ -3,8 +3,9 @@
 package org.ksharp.lsp
 
 import org.eclipse.lsp4j.launch.LSPLauncher
+import org.ksharp.common.annotation.KoverIgnore
 
-
+@KoverIgnore(reason = "Main function is not tested")
 fun main() {
     val lsp = KSharpLanguageServer()
     val launcher = LSPLauncher.createServerLauncher(lsp, System.`in`, System.out)
