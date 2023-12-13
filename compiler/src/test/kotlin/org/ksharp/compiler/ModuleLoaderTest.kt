31b21c2d22a1f9a7ad0b7f72e13b837b12630ad3
fernandojerez <fernandojerez@gmail.com>
2023-12-12 21:39:52 -0500

    add ModuleInfoLoader test

--- compiler/src/test/kotlin/org/ksharp/compiler/ModuleLoaderTest.kt
+++ compiler/src/test/kotlin/org/ksharp/compiler/ModuleLoaderTest.kt
@@ -5,13 +5,12 @@ import io.kotest.engine.spec.tempdir
 import io.kotest.matchers.booleans.shouldBeTrue
 import io.kotest.matchers.collections.shouldBeEmpty
 import io.kotest.matchers.maps.shouldBeEmpty
+import io.kotest.matchers.nulls.shouldNotBeNull
 import io.kotest.matchers.shouldBe
 import io.kotest.matchers.types.shouldBeInstanceOf
 import org.ksharp.common.new
-import org.ksharp.compiler.loader.DirectorySourceLoader
-import org.ksharp.compiler.loader.Module
-import org.ksharp.compiler.loader.ModuleLoader
-import org.ksharp.compiler.loader.ModuleLoaderErrorCode
+import org.ksharp.compiler.loader.*
+import org.ksharp.module.ModuleInfo
 import org.ksharp.module.prelude.preludeModule
 import org.ksharp.test.shouldBeLeft
 import org.ksharp.test.shouldBeRight
@@ -55,6 +54,17 @@ class ModuleLoaderTest : StringSpec({
                     entry.value.types.toFunctionType(it.info.typeSystem).representation
                 }.shouldBe(mapOf("ten/0" to "(Unit -> (Num numeric<Long>))"))
                 it.executable.execute("ten/0").shouldBe(10L)
+            }
+
+        loader2.moduleInfoLoader.load("ten", "")
+            .shouldNotBeNull()
+            .apply {
+                shouldBeInstanceOf<ModuleInfo>()
+                dependencies.shouldBeEmpty()
+                impls.shouldBeEmpty()
+                functions.mapValues { entry ->
+                    entry.value.types.toFunctionType(typeSystem).representation
+                }.shouldBe(mapOf("ten/0" to "(Unit -> (Num numeric<Long>))"))
             }
     }
 
