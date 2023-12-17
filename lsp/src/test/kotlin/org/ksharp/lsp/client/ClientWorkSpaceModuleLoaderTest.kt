35ca1167d8288f5ff24fb6cb42704702ff17b946
Fernando Jerez <fernandojerez@gmail.com>
2023-12-17 08:45:20 -0500

    add test (#187)

--- lsp/src/test/kotlin/org/ksharp/lsp/client/ClientWorkSpaceModuleLoaderTest.kt
+++ lsp/src/test/kotlin/org/ksharp/lsp/client/ClientWorkSpaceModuleLoaderTest.kt
@@ -2,7 +2,9 @@ package org.ksharp.lsp.client
 
 import io.kotest.core.spec.style.StringSpec
 import io.kotest.engine.spec.tempfile
+import io.kotest.matchers.nulls.shouldBeNull
 import io.kotest.matchers.nulls.shouldNotBeNull
+import java.net.URI
 
 class ClientWorkSpaceModuleLoaderTest : StringSpec({
     val tmpWorkSpace = tempfile("ksharp-test")
@@ -12,5 +14,12 @@ class ClientWorkSpaceModuleLoaderTest : StringSpec({
         )
         ClientWorkspaceModuleLoader.moduleLoader
             .shouldNotBeNull()
+    }
+    "Create moduleloader from a not existing Path" {
+        ClientWorkspaceModuleLoader.setWorkspaceFolder(
+            URI("file:///hello")
+        )
+        ClientWorkspaceModuleLoader.moduleLoader
+            .shouldBeNull()
     }
 })
