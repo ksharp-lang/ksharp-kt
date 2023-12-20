e6c2d26d914a8daefb891e71a5c3d248eb7ed6f9
fernandojerez <fernandojerez@gmail.com>
2023-12-20 06:47:04 -0500

    remove unused method

--- compiler/src/test/kotlin/org/ksharp/compiler/transpiler/TranspilerBaseTest.kt
+++ compiler/src/test/kotlin/org/ksharp/compiler/transpiler/TranspilerBaseTest.kt
@@ -6,16 +6,6 @@ import org.ksharp.module.prelude.preludeModule
 import org.ksharp.test.shouldBeRight
 
 class TranspilerBaseTest : StringSpec({
-    "Extract type from a abstraction" {
-        """
-            sum a :: (Num a) -> (Num a) -> (Num a)
-            sum a b = a + b
-        """.trimIndent()
-            .moduleInfo("tst.ks", preludeModule)
-            .flatMap {
-                it.artifact.abstractions.first().type
-            }.map { it.representation }.shouldBeRight("((Num a) -> (Num a) -> (Num a))")
-    }
     "Detect if a abstraction is parametric" {
         """
             sum a :: (Num a) -> (Num a) -> (Num a)
