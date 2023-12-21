15c07b1a270ee754590f8414db16cf0a787baf89
fernandojerez <fernandojerez@gmail.com>
2023-12-21 05:26:57 -0500

    add assertions

--- docs/src/test/kotlin/org/ksharp/doc/DocModuleTest.kt
+++ docs/src/test/kotlin/org/ksharp/doc/DocModuleTest.kt
@@ -1,6 +1,7 @@
 package org.ksharp.doc
 
 import io.kotest.core.spec.style.StringSpec
+import io.kotest.matchers.nulls.shouldBeNull
 import io.kotest.matchers.shouldBe
 import org.ksharp.common.io.bufferView
 import java.io.ByteArrayInputStream
@@ -34,13 +35,15 @@ class DocModuleTest : StringSpec({
                     "abs/1",
                     "Trait",
                     "Long -> Long",
-                    "Return the absolute value of a number"
+                    "Return the absolute value of a number, using trait"
                 )
             )
         ).apply {
+            representation("abs/3").shouldBeNull()
             representation("abs/1").shouldBe("Int -> Int")
             representation("sum/2").shouldBe("Int -> Int -> Int")
             representation("abs/1", "Trait").shouldBe("Long -> Long")
+            documentation("abs/1", "Trait").shouldBe("Return the absolute value of a number, using trait")
         }
     }
     "Serialize DocModule" {
