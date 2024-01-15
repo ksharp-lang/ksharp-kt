967fb871d5b4970a0a7b04efd775c5dd53131132
Fernando Jerez <fernandojerez@gmail.com>
2024-01-15 07:28:45 -0500

    remove unused method (#189)

--- compiler/src/main/kotlin/org/ksharp/compiler/transpiler/SemanticNodes.kt
+++ compiler/src/main/kotlin/org/ksharp/compiler/transpiler/SemanticNodes.kt
@@ -1,19 +1,11 @@
 package org.ksharp.compiler.transpiler
 
 import org.ksharp.common.ErrorOrValue
-import org.ksharp.common.Location
 import org.ksharp.nodes.semantic.AbstractionNode
 import org.ksharp.nodes.semantic.SemanticInfo
-import org.ksharp.nodes.semantic.SemanticNode
-import org.ksharp.typesystem.ErrorOrType
 import org.ksharp.typesystem.types.parameters
 
-val SemanticNode<SemanticInfo>.type: ErrorOrType
-    get() =
-        this.info.getInferredType(Location.NoProvided)
-
-
 val AbstractionNode<SemanticInfo>.parametric: ErrorOrValue<Boolean>
-    get() = type.map {
+    get() = info.getInferredType(location).map {
         it.parameters.firstOrNull() != null
     }
